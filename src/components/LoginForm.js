90c7885cb5bc97959cae73aaf36733184f7e433b
TheKipmaster <fkipman@hotmail.com>
2018-07-10 17:38:42 -0300

    Implementa componente 'LoginForm'

---
+++ src/components/LoginForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import { TextInput } from 'react-native';
+import { Card, CardItem, Button } from './common';
+
+class LoginForm extends Component {
+  // state = { text: '' };
+
+  render() {
+    return (
+      <Card>
+        <CardItem />
+
+        <CardItem />
+
+        <CardItem>
+          <Button>Login</Button>
+        </CardItem>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
