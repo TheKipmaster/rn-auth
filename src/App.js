c18b72931ff72b1792d87dcb70c779ce8aa2dde1
TheKipmaster <fkipman@hotmail.com>
2018-07-10 17:39:50 -0300

    Configuração do firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAsHsN_SXygUNk59qWb5f_FKMpofir0zU4',
+      authDomain: 'auth-bbc28.firebaseapp.com',
+      databaseURL: 'https://auth-bbc28.firebaseio.com',
+      projectId: 'auth-bbc28',
+      storageBucket: 'auth-bbc28.appspot.com',
+      messagingSenderId: '675264302999',
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>Hi!</Text>
+        <Header title="Authentication" />
+        <LoginForm />
       </View>
     );
   }
