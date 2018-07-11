f6c939836f88a33d2f6c54e10a0767561cb8ec65
TheKipmaster <fkipman@hotmail.com>
2018-07-11 12:59:49 -0300

    Adds secureTextEntry prop for password fields. Also changes styling slightly

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText, placeholder }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
+        secureTextEntry={secureTextEntry}
         placeholder={placeholder}
         autoCorrect={false}
         style={inputStyle}
@@ -25,12 +26,13 @@ const styles = {
     paddingLeft: 5,
     fontSize: 18,
     lineHeight: 23,
-    flex: 4
+    flex: 3
   },
   labelStyle: {
     fontSize: 18,
-    paddingLeft: 20,
-    flex: 1
+    paddingLeft: 10,
+    flex: 1,
+    // textAlign: 'center'
   },
   containerStyle: {
     height: 40,
